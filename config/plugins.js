9f9d636fbfee88605ae5ae06984c439116baeb8e
Felix-Asante <quamzgraphix@gmail.com>
2023-02-05 00:37:25 +0100

    find event bys slug

--- config/plugins.js
+++ config/plugins.js
@@ -25,6 +25,17 @@ module.exports = ({ env }) => ({
       },
     },
   },
+  slugify: {
+    enabled: true,
+    config: {
+      contentTypes: {
+        blog: {
+          field: "slug",
+          references: "title",
+        },
+      },
+    },
+  },
   ckeditor: true,
   email: {
     config: {
